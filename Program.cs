0b905138a606478e025ea611ded3d78974a2e780
Thiago Santos <Thiago_S_Sousa@outlook.com>
2024-07-31 19:18:45 -0300

    feat: summation method applied to the system

--- Program.cs
+++ Program.cs
@@ -5,7 +5,24 @@ namespace Calculator{
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Sum();
+        }
+
+        static void Sum()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Calculator, calcular nunca foi tão fãcil!");
+            Console.WriteLine("Primeiro valor: ");
+            double firstValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            double secondValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(" ");
+
+            double result = firstValue + secondValue;
+            Console.WriteLine($"O resultado da soma é: {result}");
         }
     }
 }
